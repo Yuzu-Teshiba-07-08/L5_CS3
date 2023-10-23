d00a5faa1c05660fb8cbed7160b37af059e0de02
teshibayuzu <2111140026r@ed.fuk.kindai.ac.jp>
2023-10-23 07:04:05 +0000

    CS2-2

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -8,12 +8,20 @@ class TopController < ApplicationController
         end
     end
         def login
-            if params[:uid]=="kindai" and params[:pass]=="sanriko"
+            logger.debug params[:uid]
+            if User.find_by(uid: params[:uid]) and user.find_by(pass: params[:pass])
+                #params[:uid]=="kindai" and params[:pass]=="sanriko"
                 session[:login_uid]=params[:uid]
                 redirect_to top_main_path
             else
                 render "error"
+                #logger.debug "error"
+                #render plain:"エラーです"
             end
+        end
+        def logout
+            session.delete(:login_uid)
+            redirect_to top_main_path
         end
         
 end
